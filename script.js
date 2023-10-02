6d72eef60b4fac5fb817cb013a527a084dee4c61
Daniilsa07 <lfybbk0604@mail.ru>
2023-10-02 17:44:43 +0300

    changed scripts

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let username = prompt("Свой никнейм: ")
 let years = parseInt(prompt("Свой возраст: "))
 alert("Привет, " + username + ", тебе " + years + " лет.")
+let myAdmin = confirm("Вы робот?")
+if (myAdmin)
+{
+    alert("Добро пожаловать на сайт!")
+}
+else
+{
+    close()
+}
